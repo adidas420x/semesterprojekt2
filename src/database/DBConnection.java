82541723a19026a37cfa3a22f694c717fccc665d
adidas420x <58811906+adidas420x@users.noreply.github.com>
2021-05-13 09:35:08 +0200

    Update DBConnection.java

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -33,6 +33,96 @@ public class DBConnection {
 				serverAddress, serverPort, dbName, userName, password);
 		try {
 			Class.forName(driverClass);
+			connection = DriverManager.getConnection(connectionString);
+		} catch (ClassNotFoundException e) {
+			System.err.println("Could not load JDBC driver");
+			e.printStackTrace();
+		} catch(SQLException e) {
+			System.err.println("Could not connect to database " + dbName + "@" + serverAddress + ":" + serverPort + " as user " + userName + " using password ******");
+			System.out.println("Connection string was: " + connectionString.substring(0, connectionString.length() - password.length()) + "....");
+			e.printStackTrace();
+		}
+	}
+	
+	public static DBConnection getInstance() {
+		if(dbConnection == null) {
+			dbConnection = new DBConnection();
+		}
+		return dbConnection;
+	}
+	
+	// ved ikke om dette skal bruges
+	
+	public void startTransaction() throws SQLException {
+		connection.setAutoCommit(false);
+	}
+	
+	public void commitTransaction() throws SQLException {
+		connection.commit();
+		connection.setAutoCommit(true);
+	}
+	
+	public void rollbackTransaction() throws SQLException {
+		connection.rollback();
+		connection.setAutoCommit(true);
+	}
+	
+	//hertil
+	
+	public int executeInsertWithIdentity(PreparedStatement ps) throws SQLException{
+		int res = -1;
+		try {
+			res = ps.executeUpdate();
+			if(res > 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+				rs.next();
+				res = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return res;
+	}
+	
+	public int executeInsertWithIdentity(String sql) throws SQLException {
+		System.out.println("DBConnection, Inserting: " + sql);
+		int res = -1;
+		try(Statement s = connection.createStatement()) {
+			res = s.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+			if(res > 0) {
+				ResultSet rs = s.getGeneratedKeys();
+				rs.next();
+				res = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return res;
+	}
+	
+	public int executeUpdate(String sql) throws SQLException {
+		System.out.println("DBConnection, Updating: " + sql);
+		int res = -1;
+		try(Statement s = connection.createStatement()) {
+			res = s.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return res;
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	public void disconnect() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 	
