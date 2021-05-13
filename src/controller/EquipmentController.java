8a43ad99f79ef9767adbd3e7f58a68755784d2c3
HyggenHD <75610123+HyggenHD@users.noreply.github.com>
2021-05-13 09:26:36 +0200

    ssss

--- src/controller/EquipmentController.java
+++ src/controller/EquipmentController.java
@@ -1,5 +1,24 @@
 package controller;
 
+import database.DataAccessException;
+import database.EquipmentDB;
+import database.EquipmentDBIF;
+import model.Equipment;
+
 public class EquipmentController {
 
+	private EquipmentDBIF equipmentDB;
+	
+	public EquipmentController() throws DataAccessException {
+		super();
+		this.equipmentDB = new EquipmentDB();
+	}
+	
+	public Equipment searchEquipment(String name, String eqID) {
+		return EquipmentDB.findEquipment(name, eqID);
+	}
+	
+	public Equipment addEquipmentToOrder(String eqID, quantity??) {
+		return null;
+	}
 }
