9fb11ba2c657d199f4e5b677e4a31d7092f4a6ac
adidas420x <58811906+adidas420x@users.noreply.github.com>
2021-05-13 09:00:27 +0200

    s

--- src/database/OrderDB.java
+++ src/database/OrderDB.java
@@ -1,5 +1,12 @@
 package database;
 
-public class OrderDB {
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class OrderDB implements OrderDBIF {
+
+	private static final String insertOrderQ = "insert into orders eventstartdate = ?, eventenddate = ?, eventstatus = ?, employeeid = ?";
+
+	private PreparedStatement insertOrder;
 }
