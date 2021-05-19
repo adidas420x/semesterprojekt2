6ad491cafd24a5e645fd68333c3809325543aba4
HyggenHD <75610123+HyggenHD@users.noreply.github.com>
2021-05-19 09:06:56 +0200

    Update PersonController.java

--- src/controller/PersonController.java
+++ src/controller/PersonController.java
@@ -1,5 +1,25 @@
 package controller;
 
+import database.DataAccessException;
+import database.PersonDB;
+import database.PersonDBIF;
+import model.Employee;
+import model.Person;
+
 public class PersonController {
+
+	private PersonDBIF personDB;
+	
+	public PersonController() throws DataAccessException {
+		this.personDB = new PersonDB();
+	}
+	
+	public Person findCustomerByPhone(String phoneNo) throws DataAccessException {
+		return personDB.findCustomerByPhone(phoneNo);
+	}
+
+	public Employee findEmployeeByID(String employeeID) throws DataAccessException {
+		return personDB.findEmployeeByID(employeeID);
+	}
 
 }
