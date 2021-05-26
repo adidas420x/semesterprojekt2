24622d10d8076fa71642a947ef6159bbac2909e0
StjernRogue <35777208+StjernRogue@users.noreply.github.com>
2021-05-26 22:12:58 +0200

    .

---
+++ src/test/createOrderTest.java
@@ -0,0 +1,30 @@
+//package test;
+//
+//import org.junit.*;
+//import static org.junit.Assert.*;
+//
+//import controller.*;
+//import model.*;
+//
+//public class createOrderTest {
+//	
+//	@before
+//	public void setUp() {
+//		
+//	}
+//	
+//	
+//	@test
+//	public void testCreateOrder{
+//		
+//		//Arrange
+//			
+//		
+//		//Act
+//		
+//		
+//		//Assert
+//		
+//	
+//	}
+//}
